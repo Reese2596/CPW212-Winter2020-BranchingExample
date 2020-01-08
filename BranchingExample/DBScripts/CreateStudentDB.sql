7e540c50832e15417e38c930e39aa7d77d4e4872
Reese2596 <reese2596@student.cptc.edu>
2020-01-08 14:24:45 -0800

    Created a Student DB Script.

---
+++ BranchingExample/DBScripts/CreateStudentDB.sql
@@ -0,0 +1,15 @@
+﻿
+Create Database Student
+Go
+
+Use Student
+Go
+
+Create Table Student
+(
+	 Firstname		varchar(22)		Not Null
+	,Lastname		varchar(22)		Not Null
+	,DateOfBirth	Date			Null
+	,CellPhone		char(10)		Null
+)
+Go
